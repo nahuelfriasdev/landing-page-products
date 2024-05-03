a8364cbdeae26636fad580bdaf2004ab9d8247bb
Dir Pers Mil - VS EC Nahuel Ezequiel Abel Frias <nfrias>
2024-05-03 12:44:56 -0300

    dando estructura a la landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'products/:id', component: ProductDetailComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '**', redirectTo: ''}
+];
